15376ffb3fc40fc82d261416f4e6d4660db4391f
dalto135 <67397294+dalto135@users.noreply.github.com>
2023-08-12 18:58:36 -0400

    module 8

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -4,7 +4,8 @@ import { ProductCategory } from '../product-categories/product-category';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
-import { EMPTY, Observable, catchError } from 'rxjs';
+import { BehaviorSubject, EMPTY, Observable, Subject, catchError, combineLatest, map, startWith } from 'rxjs';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -14,9 +15,27 @@ import { EMPTY, Observable, catchError } from 'rxjs';
 export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories: ProductCategory[] = [];
 
-  products$ = this.productService.products$
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();
+
+  products$ = combineLatest([
+    this.productService.productsWithCategory$,
+    this.categorySelectedAction$
+  ])
+  .pipe(
+    map(([products, selectedCategoryId]) => 
+      products.filter(product =>
+        selectedCategoryId ? product.categoryId === selectedCategoryId : true
+      )
+    ),
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  )
+
+  categories$ = this.productCategoryService.productCategories$
     .pipe(
       catchError(err => {
         this.errorMessage = err;
@@ -24,13 +43,13 @@ export class ProductListComponent {
       })
     );
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private productCategoryService: ProductCategoryService) { }
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.categorySelectedSubject.next(+categoryId);
   }
 }
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, combineLatest, map, Observable, tap, throwError } from 'rxjs';
 
 import { Product } from './product';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,22 @@ export class ProductService {
       tap(data => console.log('Products: ', JSON.stringify(data))),
       catchError(this.handleError)
     );
+
+  productsWithCategory$ = combineLatest([
+    this.products$,
+    this.productCategoryService.productCategories$
+  ]).pipe(
+    map(([products, categories]) =>
+      products.map(product => ({
+        ...product,
+        price: product.price ? product.price * 1.5 : 0,
+        category: categories.find(c => product.categoryId === c.id)?.name,
+        searchKey: [product.productName]
+      } as Product))
+    )
+  )
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private productCategoryService: ProductCategoryService) { }
 
   private fakeProduct(): Product {
     return {
@@ -50,3 +65,7 @@ export class ProductService {
   }
 
 }
+function combinedLatest(arg0: (Observable<Product[]> | Observable<import("../product-categories/product-category").ProductCategory[]>)[]) {
+  throw new Error('Function not implemented.');
+}
+
